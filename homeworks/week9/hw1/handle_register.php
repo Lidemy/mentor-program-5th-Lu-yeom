1d2edc46561cdd17aab0b3f7ab2d181f1e2f3000
Lu-yeom <2051432yoru@gmail.com>
2021-08-02 22:23:20 +0800

    done week14

---
+++ homeworks/week9/hw1/handle_register.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+  require_once('conn.php');
+
+  if (
+    empty($_POST['nickname']) ||
+    empty($_POST['username']) ||
+    empty($_POST['password']) 
+  ) {
+    header('Location: register.php?errCode=1');
+    die();
+  }
+
+  $nickname = $_POST['nickname'];
+  $username = $_POST['username'];
+  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+  $sql = sprintf(
+    "insert into luyeom_users(nickname, username, password) values('%s', '%s', '%s')",
+    $nickname,
+    $username,
+    $password
+  );
+  $result = $conn->query($sql);
+  if (!$result) {
+    $code = $conn->errno;
+    if ($code === 1062) {
+      header('Location: register.php?errCode=2');
+    }
+    die($conn->error);
+  }
+
+  header("Location: index.php");
+?>
